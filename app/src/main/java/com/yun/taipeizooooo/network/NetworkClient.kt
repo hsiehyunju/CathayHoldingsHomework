ddba7d7bf43dea148040e698440f5bebf52ed47f
yunju <hsiehyunju@gmail.com>
2025-04-09 16:57:48 +0800

    新增 network client

---
+++ app/src/main/java/com/yun/taipeizooooo/network/NetworkClient.kt
@@ -0,0 +1,25 @@
+package com.yun.taipeizooooo.network
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class NetworkClient {
+    val client = HttpClient(OkHttp) {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    prettyPrint = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+        install(Logging) {
+            level = LogLevel.BODY
+        }
+    }
+}
