7673f99c07d626655cbd0cae9422598d8ebbd79b
yunju <hsiehyunju@gmail.com>
2025-04-09 17:27:24 +0800

    添加 Application 加上 start koin

---
+++ app/src/main/java/com/yun/taipeizooooo/TaipeiZooApplication.kt
@@ -0,0 +1,30 @@
+ package com.yun.taipeizooooo
+
+import android.app.Application
+import com.yun.taipeizooooo.network.networkModule
+import com.yun.taipeizooooo.network.repositoriesModule
+import com.yun.taipeizooooo.network.useCasesModule
+import com.yun.taipeizooooo.network.viewModelsModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class TaipeiZooApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initKoin()
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidContext(this@TaipeiZooApplication)
+            modules(
+                networkModule,           // 你的 module 清單
+                repositoriesModule,
+                useCasesModule,
+                viewModelsModule
+            )
+        }
+    }
+
+}
