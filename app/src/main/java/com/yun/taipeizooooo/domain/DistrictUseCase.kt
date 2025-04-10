3ae6337e341eaea41e121370f6ee648b1d0058aa
yunju <hsiehyunju@gmail.com>
2025-04-10 15:18:13 +0800

    移除不必要的 import

--- app/src/main/java/com/yun/taipeizooooo/domain/DistrictUseCase.kt
+++ app/src/main/java/com/yun/taipeizooooo/domain/DistrictUseCase.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.mapLatest
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
