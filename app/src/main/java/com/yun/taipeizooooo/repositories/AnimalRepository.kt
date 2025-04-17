d613986e1374dac993674f748621ed15d3ea501f
yunju <hsiehyunju@gmail.com>
2025-04-17 10:34:59 +0800

    新增 Plant 跟 Anima repo

---
+++ app/src/main/java/com/yun/taipeizooooo/repositories/AnimalRepository.kt
@@ -0,0 +1,28 @@
+ package com.yun.taipeizooooo.repositories
+
+import com.yun.taipeizooooo.models.DistrictResponse
+import com.yun.taipeizooooo.models.RequestData
+import com.yun.taipeizooooo.network.ApiService
+
+ /**
+  * AnimalRepository 用來處理與動物園管區內動物的資料請求
+  *
+  * @param apiService ApiService 用來發送 API 請求
+  */
+ class AnimalRepository(
+    private val apiService: ApiService
+) {
+    /**
+     * Fetches the list of animal from the Taipei Zoo API.
+     *
+     * @param offset The offset for pagination.
+     * @param limit The limit for pagination.
+     */
+    suspend fun fetchAnimals(
+        offset: Int,
+        limit: Int
+    ): DistrictResponse = apiService.get(
+        url = "https://data.taipei/api/v1/dataset/6afa114d-38a2-4e3c-9cfd-29d3bd26b65b?scope=resourceAquire",
+        request = RequestData(offset, limit)
+    )
+}
---
+++ app/src/main/java/com/yun/taipeizooooo/repositories/PlantRepository.kt
@@ -0,0 +1,28 @@
+ package com.yun.taipeizooooo.repositories
+
+import com.yun.taipeizooooo.models.DistrictResponse
+import com.yun.taipeizooooo.models.RequestData
+import com.yun.taipeizooooo.network.ApiService
+
+ /**
+  * PlantRepository 用來處理與動物園管區內植物的資料請求
+  *
+  * @param apiService ApiService 用來發送 API 請求
+  */
+ class PlantRepository(
+    private val apiService: ApiService
+) {
+    /**
+     * Fetches the list of plant from the Taipei Zoo API.
+     *
+     * @param offset The offset for pagination.
+     * @param limit The limit for pagination.
+     */
+    suspend fun fetchPlant(
+        offset: Int,
+        limit: Int
+    ): DistrictResponse = apiService.get(
+        url = "https://data.taipei/api/v1/dataset/e20706d8-bf89-4e6a-9768-db2a10bb2ba4?scope=resourceAquire",
+        request = RequestData(offset, limit)
+    )
+}
